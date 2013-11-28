d950d77bd3e960bf917aa91ae253e76ab05af6ea
waddlesplash <ajcsweb@gmail.com>
2013-11-27 21:07:56 -0500

    Missed that file.

---
+++ Debug/BeTrisDebug.h
@@ -0,0 +1,10 @@
+#ifndef _BETRISDEBUG_H
+#define _BETRISDEBUG_H
+
+#include "BeTrisConstants.h"
+
+// mode debug ou pas
+//#define		BETRIS_DEBUG_MODE			BETRIS_DEBUG_MODE_ALL
+#define		BETRIS_DEBUG_MODE			BETRIS_DEBUG_NONE
+
+#endif
